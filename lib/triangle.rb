0475a28593e610d329fad2e59bbf7e20ab3945f3
Loren Cipriano <loren25cipriano@gmail.com>
2020-12-11 12:31:27 -0800

    commit tree b6e125b98f19fe7579cd912a802872cc330473a9

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,51 @@
 class Triangle
   # write code here
+
+  attr_accessor :side1, :side2, :side3
+
+  def initialize( side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end 
+
+  # def kind
+  #   if side1 == side2 && side3 == side1
+  #     :equilateral
+  #   elsif side1 == side2 || side2 == side3 || side3 == side1 
+  #     :isosceles
+  #   else
+  #     :scalene
+  #   end 
+  # end 
+
+
+  def kind 
+    if invalid_triangle
+      raise TriangleError
+    elsif side1 == side2 && side3 == side1
+      :equilateral
+    elsif side1 == side2 || side2 == side3 || side3 == side1 
+      :isosceles
+    else
+      :scalene
+    end 
+  end 
+
+  def invalid_triangle 
+    if side1 * side2 * side3 == 0 || side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2
+      raise TriangleError 
+    end 
+  end 
+
+
+  class TriangleError < StandardError
+    # triangle error code
+    puts "Not a triangle"
+  end
 end
+
+
+# 1. `:equilateral`
+# 2. `:isosceles`
+# 3. `:scalene`
